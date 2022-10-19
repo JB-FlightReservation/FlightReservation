d12fa3ec878b33cd194ec25ab7fc73b97518e60b
sohyun03 <sohun1103@naver.com>
2022-10-19 17:07:57 +0900

    project upload

---
+++ src/main/java/com/joongbu/flight_reservation/JbFlightReservationApplication.java
@@ -0,0 +1,13 @@
+package com.joongbu.flight_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbFlightReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbFlightReservationApplication.class, args);
+	}
+
+}
