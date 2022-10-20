69ae73d2072721498394151c00bf2ffe7d211066
송은지 <sssh000508@gmail.com>
2022-10-20 16:57:35 +0900

    join

--- src/main/java/com/joongbu/flight_reservation/controller/test1.java
+++ src/main/java/com/joongbu/flight_reservation/controller/test1.java
@@ -1,5 +1,14 @@
 package com.joongbu.flight_reservation.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class test1 {
+	
+	@GetMapping("/signup/joinPage.do")
+	public String joinpage() {
+		return "/signup/joinPage";
+	}
 
 }
