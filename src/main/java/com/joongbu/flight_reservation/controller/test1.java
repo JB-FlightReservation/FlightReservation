76ed4e9948da6249f43f18e1c8ecc59d8a46e175
youn <oonyoun@gmail.com>
2022-10-22 17:38:17 +0900

    로그인페이지

--- src/main/java/com/joongbu/flight_reservation/controller/test1.java
+++
@@ -1,5 +0,0 @@
-package com.joongbu.flight_reservation.controller;
-
-public class test1 {
-
-}
---
+++ src/main/java/com/joongbu/flight_reservation/dto/CustomerDto.java
@@ -0,0 +1,35 @@
+package com.joongbu.flight_reservation.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+/*
++-----------+--------------+------+-----+---------+----------------+
+| Field     | Type         | Null | Key | Default | Extra          |
++-----------+--------------+------+-----+---------+----------------+
+| ct_no     | int unsigned | NO   | PRI | NULL    | auto_increment |
+| ct_id     | varchar(45)  | NO   | UNI | NULL    |                |
+| ct_pw     | varchar(45)  | NO   |     | NULL    |                |
+| ct_name   | varchar(45)  | NO   |     | NULL    |                |
+| ct_email  | varchar(45)  | NO   |     | NULL    |                |
+| ct_phone  | varchar(45)  | NO   |     | NULL    |                |
+| ct_birth  | date         | NO   |     | NULL    |                |
+| ct_gender | char(1)      | NO   |     | NULL    |                |
+| ct_create | date         | NO   |     | NULL    |                |
+| oc_no     | int unsigned | NO   | UNI | NULL    |                |
++-----------+--------------+------+-----+---------+----------------+
+ */
+@Data
+public class CustomerDto {
+	private int ctNo;
+	private String ctId;
+	private String ctPw;
+	private String ctName;
+	private String ctEmail;
+	private String ctPhone;
+	private Date ctBirth;
+	private char ctGender;
+	private Date ctCreate;
+	private int ocNo;
+}
