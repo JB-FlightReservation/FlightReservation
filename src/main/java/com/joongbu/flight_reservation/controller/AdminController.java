a30bb3dfb72e26c9e9dfdd2b34d05a155d1e21dc
sohyun03 <sohun1103@naver.com>
2022-10-27 01:09:43 +0900

    오류 수정

--- src/main/java/com/joongbu/flight_reservation/controller/AdminController.java
+++ src/main/java/com/joongbu/flight_reservation/controller/AdminController.java
@@ -1,176 +1,173 @@
 
 package com.joongbu.flight_reservation.controller;
 
-import com.joongbu.flight_reservation.dto.ReservationDto;
-import org.apache.catalina.connector.Request;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.joongbu.flight_reservation.dto.AdminDto;
 import com.joongbu.flight_reservation.dto.CustomerDto;
+import com.joongbu.flight_reservation.dto.ReservationDto;
 import com.joongbu.flight_reservation.mapper.AdminMapper;
 
 import lombok.Getter;
 import lombok.Setter;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttribute;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.sql.DataSource;
-import java.util.List;
-
 @RequestMapping("/adminpage")
 @Controller
 public class AdminController {
-    @Autowired
-    DataSource dataSource;
+	@Autowired
+	DataSource dataSource;
 
-    @Autowired
-    AdminMapper adminMapper;
+	@Autowired
+	AdminMapper adminMapper;
 
-    /*                          회원 관리 시작                         */
-    @GetMapping("/userManagement.do")
-    public String userManagement(Model model, @RequestParam(defaultValue = "1") int page,@RequestParam(required = false) String ctName) {
-        final int ROWS = 10;
-        int startRow = (page - 1) * ROWS;
-        List<CustomerDto> customerList = null;
-        try {
-            customerList = adminMapper.customerList(startRow, ROWS,ctName);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        model.addAttribute("customerList", customerList);
-        return "/adminpage/userManagement";
-    }
+	/* 회원 관리 시작 */
+	@GetMapping("/userManagement.do")
+	public String userManagement(Model model, @RequestParam(defaultValue = "1") int page,
+			@RequestParam(required = false) String ctName) {
+		final int ROWS = 10;
+		int startRow = (page - 1) * ROWS;
+		List<CustomerDto> customerList = null;
+		try {
+			customerList = adminMapper.customerList(startRow, ROWS, ctName);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		model.addAttribute("customerList", customerList);
+		return "/adminpage/userManagement";
+	}
 
+	@GetMapping("/customerDelete.do")
+	public String customerDelete(int ctNo) {
+		int delete = 0;
+		try {
+			delete = adminMapper.customerDelete(ctNo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (delete > 0) {
+			return "redirect:/adminpage/userManagement.do";
+		} else {
+			return "redirect:/adminpage/management.do";
+		}
+	}
 
-    @GetMapping("/customerDelete.do")
-    public String customerDelete(int ctNo) {
-        int delete = 0;
-        try {
-            delete = adminMapper.customerDelete(ctNo);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (delete > 0) {
-            return "redirect:/adminpage/userManagement.do";
-        } else {
-            return "redirect:/adminpage/management.do";
-        }
-    }
-    @PostMapping("/customerUpdate.do")
-    public String customerUpdate(CustomerDto customerDto)    {
-        int update = 0;
-        try {
-            update = adminMapper.customerUpdate(customerDto);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println(update);
-        if (update > 0) {
-            return "redirect:/adminpage/userManagement.do";
-        } else {
-            return "redirect:/adminpage/management.do";
-        }
-    }
-    /*                          회원 관리 끝                           */
+	@PostMapping("/customerUpdate.do")
+	public String customerUpdate(CustomerDto customerDto) {
+		int update = 0;
+		try {
+			update = adminMapper.customerUpdate(customerDto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(update);
+		if (update > 0) {
+			return "redirect:/adminpage/userManagement.do";
+		} else {
+			return "redirect:/adminpage/management.do";
+		}
+	}
+	/* 회원 관리 끝 */
 
-    /*                          예약 관리 시작                         */
-    @GetMapping("/reservationManagement.do")
-    public String reservationManagement(Model model, @RequestParam(defaultValue = "1") int page, @RequestParam(required = false) Integer ctNo) {
+	/* 예약 관리 시작 */
+	@GetMapping("/reservationManagement.do")
+	public String reservationManagement(Model model, @RequestParam(defaultValue = "1") int page,
+			@RequestParam(required = false) Integer ctNo) {
 
-        final int ROWS = 10;
-        int startRow = (page - 1) * ROWS;
-        List<ReservationDto> reservationList = null;
-        try {
-            reservationList = adminMapper.reservationList(startRow, ROWS, ctNo);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        model.addAttribute("reservationList", reservationList);
-        return "/adminpage/reservationManagement";
-    }
+		final int ROWS = 10;
+		int startRow = (page - 1) * ROWS;
+		List<ReservationDto> reservationList = null;
+		try {
+			reservationList = adminMapper.reservationList(startRow, ROWS, ctNo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		model.addAttribute("reservationList", reservationList);
+		return "/adminpage/reservationManagement";
+	}
 
+	@GetMapping("reservationDetailManagement.do")
+	public void reservationDetailManagement() {
 
-    @GetMapping("reservationDetailManagement.do")
-    public void reservationDetailManagement() {
+	}
 
+	/* 예약 관리 끝 */
+	@GetMapping("management.do")
+	public void managemen() {
+	}
 
-    }
-    /*                          예약 관리 끝                         */
-    @GetMapping("management.do")
-    public void management(){}
+	@GetMapping("createCoupon.do")
+	public void createCoupon() {
+	}
 
+	@GetMapping("coupon.do")
+	public void coupon() {
+	}
 
-    @GetMapping("createCoupon.do")
-    public void createCoupon() {
-    }
-
-    @GetMapping("coupon.do")
-    public void coupon() {
-    }
-
-    @GetMapping("counselling.do")
-    public void counselling() {
-    }
-
-}
+	@GetMapping("counselling.do")
+	public void counselling() {
+	}
 
 	@GetMapping("/loginpage.do")
-	public void loginpage(
-			HttpServletRequest req,
-			HttpSession session
-			) {
-		String refererPage=req.getHeader("Referer");
+	public void loginpage(HttpServletRequest req, HttpSession session) {
+		String refererPage = req.getHeader("Referer");
 		session.setAttribute("redirectPage", refererPage);
 	}
+
 	@PostMapping("/loginpage.do")
-	public String loginpage(
-			@RequestParam(required = true) String adminId,
-			@RequestParam(required = true) String adminPw,
-			HttpSession session,
-			@SessionAttribute(required = false) String redirectPage
-			) {
+	public String loginpage(@RequestParam(required = true) String adminId,
+			@RequestParam(required = true) String adminPw, HttpSession session,
+			@SessionAttribute(required = false) String redirectPage) {
 		AdminDto loginAdmin = null;
 		try {
 			loginAdmin = adminMapper.login(adminId, adminPw);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		if(loginAdmin!=null) {
+		if (loginAdmin != null) {
 			session.setAttribute("loginAdmin", loginAdmin);
-			if(redirectPage==null) {
+			if (redirectPage == null) {
 				return "redirect:/adminpage/management.do";
-			}else {
-				return "redirect:"+redirectPage;
+			} else {
+				return "redirect:" + redirectPage;
 			}
-		}else {
+		} else {
 			return "redirect:/adminpage/loginpage.do";
 		}
 
 	}
+
 	@GetMapping("/logout.do")
 	public String logout(HttpSession session) {
 		session.removeAttribute("loginAdmin");
 		return "redirect:/adminpage/management.do";
 	}
-	@GetMapping("/management.do")
-	public String management() {
 
-		return "/adminpage/management";
-	}
+//	@GetMapping("/management.do")
+//	public String management() {
+//
+//		return "/adminpage/management";
+//	}
+
 	@GetMapping("/detail.do")
 	public AdminDto Adetail(String adminId) {
 		AdminDto adminDto = adminMapper.Adetail(adminId);
 		return adminDto;
 	}
+
 	@PostMapping("/update.do")
 	public String Aupdate(AdminDto admin) {
 		int update = 0;
@@ -180,26 +177,27 @@ public class AdminController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		if(update>0) {
+		if (update > 0) {
 			return "redirect:/adminpage/management.do";
-		}else {
-			return "redirect:/adminpage/detail.do?adminId="+admin.getAdminId();
+		} else {
+			return "redirect:/adminpage/detail.do?adminId=" + admin.getAdminId();
 		}
 	}
-	@Getter@Setter
-	class CheckAdmin{
+
+	@Getter
+	@Setter
+	class CheckAdmin {
 		private int check;
 		private AdminDto admin;
 	}
+
 	@GetMapping("/checkAdminId.do")
-	public @ResponseBody CheckAdmin checkAdminId(
-			@RequestParam(required = true) String adminId
-			) {
+	public @ResponseBody CheckAdmin checkAdminId(@RequestParam(required = true) String adminId) {
 		CheckAdmin checkAdmin = new CheckAdmin();
 		AdminDto admin = null;
 		try {
 			admin = adminMapper.Adetail(adminId);
-			if(admin!=null) {
+			if (admin != null) {
 				checkAdmin.setCheck(1);
 				checkAdmin.setAdmin(admin);
 			}
--- src/main/java/com/joongbu/flight_reservation/mapper/AdminMapper.java
+++ src/main/java/com/joongbu/flight_reservation/mapper/AdminMapper.java
@@ -1,22 +1,35 @@
 package com.joongbu.flight_reservation.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
 import com.joongbu.flight_reservation.dto.AdminDto;
 import com.joongbu.flight_reservation.dto.CustomerDto;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
+import com.joongbu.flight_reservation.dto.ReservationDto;
 
 @Mapper
 public interface AdminMapper {
-    List<CustomerDto> customerList (int startRow, int rows);
-    //List<AdminDto> adminList (@Param(value = "start")int startRow, int rows);
+	List<CustomerDto> customerList(int startRow, int rows, String ctName);
 
-    AdminDto login(String adminId,String adminPw);
-    AdminDto Adetail(String adminId);
-    int Aupdate(AdminDto admin);
+	List<CustomerDto> customerList(int startRow, int rows);
+	// List<AdminDto> adminList (@Param(value = "start")int startRow, int rows);
 
-    int update(CustomerDto customer);
-    int delete(int ctNo);
-    int insert(CustomerDto customer);
+	int customerUpdate(CustomerDto customer);
+
+	int customerDelete(int ctNo);
+
+    List<ReservationDto> reservationList(int startRow, int rows, Integer ctNo);
+
+	AdminDto login(String adminId, String adminPw);
+
+	AdminDto Adetail(String adminId);
+
+	int Aupdate(AdminDto admin);
+
+	int update(CustomerDto customer);
+
+	int delete(int ctNo);
+
+	int insert(CustomerDto customer);
 }
