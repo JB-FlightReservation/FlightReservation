6febd228932a327ccdd326302cfd34456537143f
sohyun03 <sohun1103@naver.com>
2022-10-27 15:32:33 +0900

    수하물 front

--- src/main/resources/static/js/reservation/baggage.js
+++ src/main/resources/static/js/reservation/baggage.js
@@ -1,17 +1,20 @@
-const swiper = new Swiper('.swiper2', {
-	// Optional parameters
-	direction: 'horizontal',
-	loop: false,
-
-	// If we need pagination
-	pagination: {
-		el: '.swiper-pagination',
-	},
-
-	// Navigation arrows
+var swiper = new Swiper("#secondSwiper", {
+	slidesPerView: 'auto',
+	spaceBetween: 30,
+	centeredSlides: true,
 	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
+		nextEl: ".btn_slide_next",
+		prevEl: ".btn_slide_prev",
 	},
-	slidesPerView: '2',
 });
+
+$('input[type="range"]').rangeslider();
+
+// Destroy all plugin instances created from the
+// e.g. $('input[type="range"]') elements.
+$('input[type="range"]').rangeslider('destroy');
+
+// Update all rangeslider instances for all
+// e.g. $('input[type="range"]') elements.
+// Usefull if you changed some attributes e.g. `min` or `max` etc.
+$('input[type="range"]').rangeslider('update', true);
