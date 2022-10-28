c28de75fbc5b74ca03822eae6fcbbc114b530033
sohyun03 <sohun1103@naver.com>
2022-10-28 09:30:00 +0900

    type change

--- src/main/java/com/joongbu/flight_reservation/dto/PassengerRP.java
+++ src/main/java/com/joongbu/flight_reservation/dto/PassengerRP.java
@@ -1,5 +1,7 @@
 package com.joongbu.flight_reservation.dto;
 
+import java.util.Date;
+
 import lombok.Data;
 
 //예약할 때 데이터를 넘겨주기 위한 임시 예약 객체 - 탑승자 리스트
@@ -9,7 +11,7 @@ public class PassengerRP {
 	private String pgFirstName; // 탑승객 이름
 	private String pgLastName; // 탑승객 성
 	private int pgGender; // 탑승객 성별
-	private String pgBirth; // 탑승객 생년월일 (Date로 바꿀 예정)
+	private Date pgBirth; // 탑승객 생년월일 (Date로 바꿀 예정)
 	private String pgCountry; // 탑승객 국적
 	private int pgBaggage; // 탑승객 수화물
 }
