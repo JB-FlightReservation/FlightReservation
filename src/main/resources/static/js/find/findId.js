c20805fadece572de22db40a8bcd067b4ba70c16
youn <oonyoun@gmail.com>
2022-10-30 21:22:55 +0900

    10/30

--- src/main/resources/static/js/find/findId.js
+++ src/main/resources/static/js/find/findId.js
@@ -1,7 +1,13 @@
+//const otpNum = document.getElementById('otpNum');
+//const btnAuthNo=document.getElementById('btnAuthNo');
+
+
 $(document).on('click','#btnAuthNo',function(){
-	findIdChk();
+	send();
+	
 });
-function findIdChk(){
+
+function send(){
 	let name = $('#inputName').val();
 	let email = $('#inputEmail').val();
 	
@@ -9,14 +15,27 @@ function findIdChk(){
 		url: '/login/findId.do',
 		type: 'post',
 		data : {name : name , email :email },
-		dataType : 'json'
-	})
+		dataType : 'json',
+	});
+	$('#otpNum').attr('disabled', false);
 }
 
 //$(document).on('click','#findId',function(){
 //	mainSbmit();
 //});
 function mainSubmit(){
-	let num=$('#auth').val();
+	let num=$('#otpNum').val();
 	console.log(num);
+	
+//	$ajax({
+//		url:'/login/checkAuthNum.do',
+//		type: 'post',
+//		data : {num: num},
+//		dataType : 'json',
+//	});
+	
+	document.findIdForm.submit()
 }
+
+		
+
